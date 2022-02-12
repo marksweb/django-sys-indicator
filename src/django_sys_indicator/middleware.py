ea519f465a82727dbaf47319c6657635a700c399
Mark Walker <mark.walker@realbuzz.com>
2022-02-12 13:59:59 +0000

    Preparing package setup (#2)

--- src/django_sys_indicator/middleware.py
+++ src/django_sys_indicator/middleware.py
@@ -20,9 +20,6 @@ class SystemIndicatorMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-
         response = self.get_response(request)
 
         if not SYSTEM_INDICATOR_ENABLED:
