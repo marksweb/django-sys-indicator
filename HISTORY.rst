a1f344e52a1d3b5721d720ac75e722a9c91aefaa
Mark Walker <mark.walker@realbuzz.com>
2022-02-12 13:14:02 +0000

    Preparing package setup

---
+++ HISTORY.rst
@@ -0,0 +1,8 @@
+=======
+History
+=======
+
+unreleased
+----------
+
+* Initial release.
--- src/django_sys_indicator/apps.py
+++ src/django_sys_indicator/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DjangoSysIndicatorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_sys_indicator'
+    name = 'src.django_sys_indicator'
